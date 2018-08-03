4c7a2b8a21e2128ba9409f4b3a9f9e9b7cda3097
rafalcl <rafalclab@gmail.com>
2018-08-03 16:19:33 +0200

    Add Jackson. Add BookController return book list. Add CORS mapping.

--- src/main/java/pl/coderslab/warsztat5krk03/app/AppConfig.java
+++ src/main/java/pl/coderslab/warsztat5krk03/app/AppConfig.java
@@ -2,10 +2,18 @@ package pl.coderslab.warsztat5krk03.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"pl.coderslab.warsztat5krk03"})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+//                .allowedOrigins("http://localhost");
+    }
 }
--- src/main/java/pl/coderslab/warsztat5krk03/controller/BookController.java
+++ src/main/java/pl/coderslab/warsztat5krk03/controller/BookController.java
@@ -1,14 +1,34 @@
 package pl.coderslab.warsztat5krk03.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.warsztat5krk03.model.Book;
+import pl.coderslab.warsztat5krk03.service.MemoryBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private MemoryBookService mbs;
+
     @GetMapping("/hello")
     public String hello(){
         return "{msg : hello}";
+    }
+
+    @GetMapping("/hellobook")
+    public Book helloBook(){
+        Book b= new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
+        return b;
+    }
+
+    @GetMapping("")
+    public List<Book> bookList(){
+        return this.mbs.getList();
     }
 }
